dbd00dac51f350078d1dcfc6ed67d39346cdc524
Alice (MinaevDA) <mda.job2000@gmail.com>
2021-06-17 05:23:22 +0300

    code: добавили деление

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n';
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
+
     return 0;
 }
